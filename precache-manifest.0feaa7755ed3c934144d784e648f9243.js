4a667705638bf74840fde3f0d6c9e370d69a095a
Emma Rokofyllou <emma@Emmas-MacBook-Pro.local>
2019-09-18 16:17:27 +0100

    Updates

---
+++ precache-manifest.0feaa7755ed3c934144d784e648f9243.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08060d984398b47e8dddc1f38dbc2e68",
+    "url": "/githubpages-react/index.html"
+  },
+  {
+    "revision": "e3f8827f4f2e6c5ac2bb",
+    "url": "/githubpages-react/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f3457277180ed18f44eb",
+    "url": "/githubpages-react/static/js/2.2ec1a2de.chunk.js"
+  },
+  {
+    "revision": "e3f8827f4f2e6c5ac2bb",
+    "url": "/githubpages-react/static/js/main.dd3b8694.chunk.js"
+  },
+  {
+    "revision": "a19add1a7e95accb1e39",
+    "url": "/githubpages-react/static/js/runtime~main.ffc43135.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/githubpages-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
